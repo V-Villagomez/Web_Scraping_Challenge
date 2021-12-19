3bd34c34ee37a7a9963c3f13d4a7113419fb77a8
Vanessa Villagomez <villagomez.v@outlook.com>
2021-12-18 19:42:42 -0800

    update files

--- mission_to_mars/app/app.py
+++ mission_to_mars/app/app.py
@@ -1,16 +1,32 @@
 # import Flask, pymongo, and scrape_mars (your python file)
+from flask import Flask, render_template, redirect
+import pymongo
+from flask_pymongo import PyMongo
+import scrape_mars
 
 # Instantiate a Flask app
-
+app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
 
 # Create a base '/' route that will query your mongodb database and render the `index.html` template
-
+@app.route("/")
+def echo():
+    print('msg that shows in the terminal')
+    text_to_render = "New text!"
+    return render_template("index.html", headline=text_to_render)
 
 # Create a '/scrape' route that will create the mars collection, run your scrape() function from scrape_mars, and update the mars collection in the database
 # The route should redirect back to the base route '/' with a code 302.
-
-
+@app.route("/scrape")
+def scraper():
+    listing = mongo.db.listing
+    listing_data = scrape_mars.scrape()
+    listing.update({}, listing_data, upsert=True)
+    return redirect("/", code=302)
 
 # Run your app
+if __name__ == "__main__":
+    app.run(debug=True)
